4e6e3c415787faadc9b7b589eb1f87632251b6ff
Arsh <arsh.narkhede@gmail.com>
2024-08-20 16:35:01 -0600

    basic wireframe ready, functionality pending

---
+++ SimpleBankInterface/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace SimpleBankInterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            comboBox2 = new ComboBox();
+            textBox1 = new TextBox();
+            label5 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(21, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(205, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome, John Smith!";
+            label1.Click += label1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(527, 37);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(428, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 28);
+            label2.TabIndex = 2;
+            label2.Text = "Account: ";
+            label2.Click += label2_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.Location = new Point(385, 71);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 28);
+            label4.TabIndex = 4;
+            label4.Text = "label4";
+            label4.Click += label4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.Location = new Point(230, 71);
+            label3.Name = "label3";
+            label3.Size = new Size(100, 28);
+            label3.TabIndex = 5;
+            label3.Text = "BALANCE:";
+            label3.Click += label3_Click;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Font = new Font("Segoe UI", 12F);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(698, 117);
+            panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(comboBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label5);
+            panel2.Font = new Font("Segoe UI", 12F);
+            panel2.Location = new Point(12, 154);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(698, 219);
+            panel2.TabIndex = 7;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(190, 134);
+            button2.Name = "button2";
+            button2.Size = new Size(140, 42);
+            button2.TabIndex = 7;
+            button2.Text = "WITHDRAW";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(385, 134);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 42);
+            button1.TabIndex = 6;
+            button1.Text = "DEPOSIT";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(514, 44);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 36);
+            comboBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(280, 44);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(228, 34);
+            textBox1.TabIndex = 4;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F);
+            label5.Location = new Point(134, 47);
+            label5.Name = "label5";
+            label5.Size = new Size(92, 28);
+            label5.TabIndex = 3;
+            label5.Text = "Amount: ";
+            label5.Click += label5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(722, 396);
+            Controls.Add(panel2);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Simple Bank Interface";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label label4;
+        private Label label3;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label5;
+        private Button button1;
+        private ComboBox comboBox2;
+        private TextBox textBox1;
+        private Button button2;
+        }
+}
---
+++ SimpleBankInterface/Form1.cs
@@ -0,0 +1,114 @@
+namespace SimpleBankInterface
+{
+    public partial class Form1 : Form
+    {
+        private decimal balance;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initial balance and account number (hardcoded)
+            balance = 1000.00m;
+            comboBox1.Items.AddRange(new string[] { "1234" });
+            comboBox1.SelectedIndex = 0;
+
+            label4.Text = $"${balance} CAD";
+
+            // Setup currency dropdown
+            comboBox2.Items.AddRange(new string[] { "CAD", "USD", "MXN", "EURO" });
+            comboBox2.SelectedIndex = 0; // Default to CAD
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProcessTransaction(bool isDeposit)
+        {
+            decimal amount;
+            if (decimal.TryParse(textBox1.Text, out amount) && amount > 0)
+            {
+                // Ensure SelectedItem is not null before using it
+                var selectedCurrency = comboBox2.SelectedItem as string;
+                if (selectedCurrency == null)
+                {
+                    MessageBox.Show("Please select a currency.");
+                    return;
+                }
+
+                decimal convertedAmount = ConvertToCAD(amount, selectedCurrency);
+
+                if (isDeposit)
+                {
+                    balance += convertedAmount;
+                }
+                else
+                {
+                    balance -= convertedAmount;
+                }
+
+                label4.Text = $"${balance} CAD";
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid amount.");
+            }
+        }
+
+        private decimal ConvertToCAD(decimal amount, string currency)
+        {
+            decimal conversionRate = 1.0m;
+
+            switch (currency)
+            {
+                case "USD":
+                    conversionRate = 2.0m;
+                    break;
+                case "MXN":
+                    conversionRate = 0.1m;
+                    break;
+                case "EURO":
+                    conversionRate = 4.0m;
+                    break;
+            }
+
+            return amount * conversionRate;
+        }
+    }
+}
